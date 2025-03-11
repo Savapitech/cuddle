afd3240889def8a331a9c58b33c8bff16795e4db
ronan <ronan.botrel@epitech.eu>
2025-03-11 19:58:43 +0100

    Fix merge

--- tests/main.c
+++ tests/main.c
@@ -16,5 +16,16 @@ int main(int ac, char **av)
 
     if (dataframe == NULL)
         return 84;
+    printf("nb_columns= %d\n", dataframe->nb_columns);
+    printf("nb_rows= %d\n", dataframe->nb_rows);
+    for (int a = 0; dataframe->column_names[a] != NULL; a++) {
+        if (a == 0)
+            printf("name colmuns= %s\n", dataframe->column_names[a]);
+        else
+            printf("              %s\n", dataframe->column_names[a]);
+    }
+    my_free_array(dataframe->column_names);
+    //free(dataframe->column_names);
+    free(dataframe);
     return 0;
 }
