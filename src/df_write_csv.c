bd5cb2b6124c3781b3a6ae169e6ad3e1cf1b7b12
savalet <savinien.petitjean@gmail.com>
2025-03-16 18:29:07 +0100

    Add custom separator in df write

--- src/df_write_csv.c
+++ src/df_write_csv.c
@@ -38,13 +38,15 @@ void write_value(void *vl, FILE *file, column_type_t type)
 static
 void write_values(dataframe_t *df, FILE *file)
 {
+    char const *separator = df->separator == NULL ? "," : df->separator;
+
     for (int col = 0; col < df->nb_columns; col++)
         fprintf(file, "%s%s", df->column_names[col],
             col < df->nb_columns - 1 ? "," : "\n");
     for (int row = 0; row < df->nb_rows; row++) {
         for (int col = 0; col < df->nb_columns; col++) {
             write_value(df->data[row][col], file, df->column_type[col]);
-            fprintf(file, "%s", col < df->nb_columns - 1 ? "," : "\n");
+            fprintf(file, "%s", col < df->nb_columns - 1 ? separator : "\n");
         }
     }
 }
